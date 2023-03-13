65a3b8741547c1c518ee95ad8e42f4fd8ef34b6e
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-03-13 10:24:14 +0100

    add forward path

--- SiameseNetworks/model.py
+++ SiameseNetworks/model.py
@@ -30,7 +30,7 @@ from preprocessing import get_pretrained_vectors, get_args_and_dataloaders
 class SiameseNetwork(nn.Module):
     '''
     A siamese network model for multi-class classification on text data.
-    The two models are the same, MLP with 2 hidden layers and a last linear layer for classification.
+    The two models are the same, MLP with 1 hidden layer and a last linear layer for classification.
     '''
     def __init__(self, input_dim, hidden_dim, n_classes):
         super(SiameseNetwork, self).__init__()
@@ -39,11 +39,25 @@ class SiameseNetwork(nn.Module):
         self.ebd = torch.nn.Embedding.from_pretrained(get_pretrained_vectors(), freeze=True)
 
         # two linear layers
-        self.layer1 = torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim, bias=True)
-        self.layer2 = torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim, bias=True)
+        self.hidden_layer = torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim, bias=True)
+
+        self.drop = nn.Dropout(p=0.25)
 
         # classification layer
         self.classification_layer = torch.nn.Linear(in_features=hidden_dim, out_features=n_classes)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward_once(self, x):
+        x = self.ebd(x)
+
+        output = self.hidden_layer(x)
+        output = self.drop(output)
+        output = self.classification_layer(output)
+
+        logits = self.softmax(output)
+        return logits
+
 
 # Training and inference functions
 
