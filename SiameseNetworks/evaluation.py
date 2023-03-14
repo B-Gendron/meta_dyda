0cec0d4a2ff3a0a71e3be205ca2d3b85218ef751
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-03-14 15:04:24 +0100

    put model aside to ease eval

--- SiameseNetworks/evaluation.py
+++ SiameseNetworks/evaluation.py
@@ -1,1 +1,39 @@
 # In the file main.py, we learn a representation of the utterances which is adapted to emotion detection. Now it is time to evaluate the model by using a classifier and checking for accuracy.
+
+        # --------------------------------------------------- #
+        # --------------------- Imports --------------------- #
+        # --------------------------------------------------- #
+
+# Torch utils
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+# Data loading and preprocessing
+import json
+from datasets.dataset_dict import DatasetDict
+
+# General purposes modules
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from tqdm import tqdm
+from termcolor import colored
+
+from model import SiameseNetwork
+
+# load the model
+model = SiameseNetwork(20, 300, 7)
+model.load_state_dict(torch.load("./models/utterance_model.pt"))
+model.eval()
+
+        # ------------------------------------------------------ #
+        # --------------------- Classifier --------------------- #
+        # ------------------------------------------------------ #
+
+class EmotionsClassifier(nn.Module):
+    pass
--- SiameseNetworks/main.py
+++ SiameseNetworks/main.py
@@ -23,7 +23,8 @@ from tqdm import tqdm
 from termcolor import colored
 
 # From others files of this repo
-from preprocessing import get_pretrained_vectors, get_dyda_utterances
+from preprocessing import get_dyda_utterances
+from model import SiameseNetwork
 
         # --------------------------------------------------- #
         # ------------------ Dataset class ------------------ #
@@ -107,58 +108,6 @@ args['max_eps'] = 10
 # print(next(iter(train_loader))["anchor"].shape)
 # print("Expected output: torch.Size([64, 20])")
 
-
-        # --------------------------------------------------- #
-        # ------------------- Model class ------------------- #
-        # --------------------------------------------------- #
-
-pretrained_vectors = get_pretrained_vectors()
-
-# Instantiate the Siamese Networks (MLP with ReLU or softmax)
-class SiameseNetwork(nn.Module):
-    '''
-    A siamese network model for multi-class classification on text data.
-    The two models are the same, MLP with 1 hidden layer and a last linear layer for classification.
-    '''
-    def __init__(self, input_dim, hidden_dim, n_classes):
-        super(SiameseNetwork, self).__init__()
-
-        # embedding layer
-        self.ebd = torch.nn.Embedding.from_pretrained(pretrained_vectors.vectors, freeze=False)
-
-        # two linear layers
-        self.hidden_layer = torch.nn.Linear(in_features=hidden_dim, out_features=hidden_dim, bias=True)
-
-        self.drop = nn.Dropout(p=0.25)
-
-        # classification layer
-        self.classification_layer = torch.nn.Linear(in_features=hidden_dim, out_features=n_classes)
-
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward_once(self, x):
-        x = self.ebd(x)
-
-        output = self.hidden_layer(x)
-        output = self.drop(output)
-
-        return output
-    
-    def forward(self, input1, input2, input3):
-        A = self.forward_once(input1)
-        P = self.forward_once(input2)
-        N = self.forward_once(input3)
-        triplet_loss = nn.TripletMarginLoss(margin=1.0, p=2)
-        output = triplet_loss(A, P, N)
-
-        return output
-
-# TEST FORWARD PATH ON ONE ITERATION:
-# model = SiameseNetwork(input_dim=20, hidden_dim=300, n_classes=7)
-# data = next(iter(train_loader))
-# output = model(data["anchor"][1], data["positive"][1], data["negative"][1])
-# print(output)
-# print("Expected output: tensor(0.8057, grad_fn=<MeanBackward0>)")
 
         # --------------------------------------------------- #
         # ------------------ Training loop ------------------ #
